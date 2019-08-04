529fc2c3a92516226dc97f92b8945a17c99f17a0
Vedant Harshit <53620816+VedantHarshit@users.noreply.github.com>
2019-08-04 19:49:23 +0530

    Add files via upload

---
+++ Q6.java
@@ -0,0 +1,9 @@
+public class Q6
+{
+    public static void main(String args[])
+    {
+        /*Using formula for sum of squares of first n natural numbers and sum of first
+          n natural numbers*/
+        System.out.println((int)Math.pow((100*101)/2,2) - (100*(101)*((201))/6));
+    }
+}
