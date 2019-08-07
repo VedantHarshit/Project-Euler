c6b381b92767197c67c275ded6455e79ccd9fed8
Vedant Harshit <53620816+VedantHarshit@users.noreply.github.com>
2019-08-07 16:08:37 +0530

    Add files via upload

--- Q3.java
+++ Q3.java
@@ -3,12 +3,12 @@ public class Q3
     public static void main(String args[])
     {
        long number = 600851475143l;
-       for(int i = 2;i <= number/2;i++) 
+       for(int i = 3;i <= number/2;i+=2) 
        {
            if(number % i == 0)
            {
                number/=i;
-               i--;
+               i-=2;
            }
        }
        System.out.println(number);
